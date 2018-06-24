23d280ab8c2a0429bb5d65d20a70e3a581e0b4e1
aruieva <40498241+aruieva@users.noreply.github.com>
2018-06-24 22:09:22 +0300

    Add files via upload

--- 1/1.cpp
+++ 1/1.cpp
@@ -7,9 +7,14 @@ int main()
  
  int a;
   printf("Vichislenie proizvolnogo chisla iz ryada Fibonnachi\n\n");
+  L1:
   printf("Vvedite nomer chisla v ryade ");
   scanf_s("%i", &a);
-  
+  if (a < 0)
+  {
+	  printf("Oshibka!\n");
+	  goto L1;
+  }
   int pred = 1;
   int cur = 0;
   for (int i = 0; i < a; i++)
